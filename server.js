0066890a68754c82c0555672f15090a7e4d47cbc
Kmayer48 <kenny.mayer3@gmail.com>
2019-02-23 15:59:40 -0500

    heroku edits for deployment

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
 var exphbs = require("express-handlebars");
+var path = require("path");
 var app = express();
 var PORT = process.env.PORT || 8080;
+
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
